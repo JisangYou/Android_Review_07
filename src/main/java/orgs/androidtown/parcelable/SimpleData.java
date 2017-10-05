a85078ab6789780262661121d27a5eb9e999a095
Jisang you <jisangman89@gmail.com>
2017-10-06 01:27:57 +0900

    first commit parcelable

---
+++ src/main/java/orgs/androidtown/parcelable/SimpleData.java
@@ -0,0 +1,45 @@
+package orgs.androidtown.parcelable;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Jisang on 2017-10-06.
+ */
+
+public class SimpleData implements Parcelable {
+
+    int number;
+    String message;
+
+
+    public SimpleData(int number, String message) {
+        this.number = number;
+        this.message = message;
+    }
+
+   public SimpleData(Parcel src){
+       number = src.readInt();
+       message= src.readString();
+   }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
+        public SimpleData createFromParcel(Parcel src){
+            return new SimpleData(src);
+        }
+        public SimpleData[] newArray(int size){
+            return new SimpleData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(number);
+        parcel.writeString(message);
+    }
+}
